b4eda36b8a1cd1b7cc16171712563b0648457020
eduvedras <edu.vedras@gmail.com>
2022-06-18 21:40:53 +0100

    Resto dos triggers

--- schema.sql
+++ schema.sql
@@ -133,10 +133,24 @@ create or replace function uni_fun() returns
   trigger as $$
   declare c integer;
   begin
-    select count(*) into c from evento_reposicao  where new.unidades > max_unidades where();
+    select count(*) into c from evento_reposicao  where new.unidades > max_unidades(select unidades from planograma where ean = new.ean));
     if c > 0 then
-      raise exception 'Uma categoria não pode estar contida em si propria'
-      using hint = 'Por favor verifique o nome da categoria.';
+      raise exception 'O numero de unidades repostas num Evento de Reposição não pode exceder o número de unidades especificado no Planograma'
+      using hint = 'Por favor verifique o número de unidades a repor.';
+    end if;
+    return new;
+  end;
+$$ language plpgsql;
+
+create or replace function pro_fun() returns
+  trigger as $$
+  declare c integer;
+  begin
+    select count(*) into c from evento_reposicao  where new.ean = 
+              prodean(select ean from prateleira where nome = prodcat(select nome from tem_categoria where new.ean = ean));
+    if c = 0 then
+      raise exception 'Um Produto só pode ser reposto numa Prateleira que apresente (pelo menos) uma das Categorias desse produto'
+      using hint = 'Por favor verifique a prateleira.';
     end if;
     return new;
   end;
@@ -149,3 +163,7 @@ create trigger cat_trg before insert on tem_outra for each row execute procedure
 create trigger uni_trg before update on evento_reposicao for each row execute procedure uni_fun();
 
 create trigger uni_trg before insert on evento_reposicao for each row execute procedure uni_fun();
+
+create trigger pro_trg before update on evento_reposicao for each row execute procedure pro_fun();
+
+create trigger pro_trg before insert on evento_reposicao for each row execute procedure pro_fun();
