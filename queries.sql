acda193db3061e6e6441c77ee5ef2da273ea6b6f
rodrigomelopinto <rodrigo.melo.pinto@tecnico.ulisboa.pt>
2022-06-20 18:17:02 +0100

    alteracao queries

--- queries.sql
+++ queries.sql
@@ -4,9 +4,14 @@ select nome from retalhista where tin = tinRet (
 )
 
 /*2*/
-select nome from retalhista where tin = tinRet (
-    select tin, nome_cat from responsavel_por where nome_cat = catTin(select nome from categoria_simples) group by tin
-)
+select retalhista.nome 
+from categoria_simples join responsavel_por on categoria_simples.nome = responsavel_por.nome_cat
+join retalhista on retalhista.tin = responsavel_por.tin
+group by retalhista.nome
+
+/*igual a 2?*/
+select nome from retalhista where tin in (select tin from responsavel_por group by tin having count(*)=ALL(select count(*)
+    from categoria_simples));
 
 /*3*/
 select ean from produto where ean not in (select ean from evento_reposicao);
