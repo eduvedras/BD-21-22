e47b2cd5c9c1f590a0a066e7fae854899cf7953e
eduvedras <edu.vedras@gmail.com>
2022-06-21 10:20:35 +0100

    ICS a funcionar

--- ICS.sql
+++ ICS.sql
@@ -11,11 +11,15 @@ create or replace function cat_fun() returns
   end;
 $$ language plpgsql;
 
+create trigger cat_trg before update on tem_outra for each row execute procedure cat_fun();
+
+create trigger cat_trg_i before insert on tem_outra for each row execute procedure cat_fun();
+
 create or replace function uni_fun() returns
   trigger as $$
   declare c integer;
   begin
-    select count(*) into c from evento_reposicao  where new.unidades > max_unidades(select unidades from planograma where ean = new.ean));
+    select count(*) into c from evento_reposicao where new.unidades > (select unidades from planograma where ean = new.ean);
     if c > 0 then
       raise exception 'O numero de unidades repostas num Evento de Reposição não pode exceder o número de unidades especificado no Planograma'
       using hint = 'Por favor verifique o número de unidades a repor.';
@@ -24,12 +28,15 @@ create or replace function uni_fun() returns
   end;
 $$ language plpgsql;
 
+create trigger uni_trg before update on evento_reposicao for each row execute procedure uni_fun();
+
+create trigger uni_trg_i before insert on evento_reposicao for each row execute procedure uni_fun();
+
 create or replace function pro_fun() returns
   trigger as $$
   declare c integer;
   begin
-    select count(*) into c from evento_reposicao  where new.ean = 
-              prodean(select ean from prateleira where nome = prodcat(select nome from tem_categoria where new.ean = ean));
+    select count(*) into c from planograma  where new.ean = (select ean from tem_categoria where tem_categoria.nome = (select nome from prateleira where new.nro = nro));
     if c = 0 then
       raise exception 'Um Produto só pode ser reposto numa Prateleira que apresente (pelo menos) uma das Categorias desse produto'
       using hint = 'Por favor verifique a prateleira.';
@@ -38,14 +45,6 @@ create or replace function pro_fun() returns
   end;
 $$ language plpgsql;
 
-create trigger cat_trg before update on tem_outra for each row execute procedure cat_fun();
+create trigger pro_trg before update on planograma for each row execute procedure pro_fun();
 
-create trigger cat_trg before insert on tem_outra for each row execute procedure cat_fun();
-
-create trigger uni_trg before update on evento_reposicao for each row execute procedure uni_fun();
-
-create trigger uni_trg before insert on evento_reposicao for each row execute procedure uni_fun();
-
-create trigger pro_trg before update on evento_reposicao for each row execute procedure pro_fun();
-
-create trigger pro_trg before insert on evento_reposicao for each row execute procedure pro_fun();
+create trigger pro_trg_i before insert on planograma for each row execute procedure pro_fun();
--- view.sql
+++ view.sql
@@ -1,5 +1,24 @@
+CREATE TABLE tempo
+    (ano VARCHAR(50),
+    trimestre VARCHAR(50),
+    mes VARCHAR(50),
+    dia_mes VARCHAR(50),
+    dia_semana VARCHAR(50),
+    PRIMARY KEY(ano, trimestre, mes,dia_mes,dia_semana),
+    ano EXTRACT(YEAR from evento_reposicao.instante)),trimestre = (select EXTRACT(QUARTER from evento_reposicao.instante)),
+    mes = (select EXTRACT(MONTH from evento_reposicao.instante)), dia_mes = (select EXTRACT(DAY from evento_reposicao.instante)),
+    dia_semana = (select EXTRACT(DOW from evento_reposicao.instante)));
+
+
+
 create view vendas(ean,cat,ano,trimestre,mes,dia_mes,dia_semana,distrito,concelho,unidades) as
-select ean from produto,
+select ean, ano,trimestre,mes,dia_mes,dia_semana, distrito,concelho,unidades from produto natural join categoria
+natural join evento_reposicao natural join ponto_de_retalho;
+
+/*ano = (select EXTRACT(YEAR from instante)),trimestre = (select EXTRACT(QUARTER from instante)),
+mes = (select EXTRACT(MONTH from instante)), dia_mes = (select EXTRACT(DAY from instante)),
+dia_semana = (select EXTRACT(DOW from instante));*/
+/*
 select cat from categoria,
 select ano from EXTRACT(YEAR from evento_reposicao.instante),
 select trimestre from EXTRACT(QUARTER from evento_reposicao.instante),
@@ -7,4 +26,4 @@ select mes from EXTRACT(MONTH from evento_reposicao.instante),
 select dia_mes from EXTRACT(DAY from evento_reposicao.instante),
 select dia_semana from EXTRACT(DOW from evento_reposicao.instante),
 select distrito,concelho from ponto_de_retalho,
-select unidades from evento_reposicao;
+select unidades from evento_reposicao;*/
